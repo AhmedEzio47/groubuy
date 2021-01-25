97199d46f3c988fa4fc2f495f5ec92cbf30bb9ce
Ahmed <ahmednab93@gmail.com>
2021-01-25 22:14:23 +0200

    -Product offers view model

--- lib/pages/product_page.dart
+++ lib/pages/product_page.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:groubuy/constants/colors.dart';
 import 'package:groubuy/constants/constants.dart';
 import 'package:groubuy/database_service.dart';
-import 'package:groubuy/models/offer.dart';
 import 'package:groubuy/models/product.dart';
+import 'package:groubuy/view_models/product_offers_vm.dart';
 import 'package:groubuy/widgets/capsule_button.dart';
 import 'package:groubuy/widgets/offer_item.dart';
 import 'package:groubuy/widgets/product_item.dart';
+import 'package:provider/provider.dart';
 
 import 'new_offer.dart';
 
@@ -21,19 +22,11 @@ class ProductPage extends StatefulWidget {
 class _ProductPageState extends State<ProductPage> {
   @override
   void initState() {
-    getOffers();
+    viewModel = ProductOffersVM(widget.product);
     super.initState();
   }
 
-  List<Offer> _offers = [];
-  getOffers() async {
-    List<Offer> offers =
-        await DatabaseService.getAvailableOffersByProduct(widget.product.id);
-    setState(() {
-      _offers = offers;
-    });
-  }
-
+  ProductOffersVM viewModel;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,18 +41,22 @@ class _ProductPageState extends State<ProductPage> {
             product: widget.product,
           ),
           Expanded(
-            child: GridView.builder(
-              scrollDirection: Axis.vertical,
-              itemCount: _offers.length,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                childAspectRatio: 1.4,
-                crossAxisCount: 2,
-              ),
-              itemBuilder: (context, index) {
-                return OfferItem(
-                  offer: _offers[index],
-                );
-              },
+            child: ChangeNotifierProvider<ProductOffersVM>(
+              create: (context) => viewModel,
+              child: Consumer<ProductOffersVM>(
+                  builder: (context, model, child) => GridView.builder(
+                        scrollDirection: Axis.vertical,
+                        itemCount: viewModel.offers.length,
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          childAspectRatio: 1.4,
+                          crossAxisCount: 2,
+                        ),
+                        itemBuilder: (context, index) {
+                          return OfferItem(
+                            offer: viewModel.offers[index],
+                          );
+                        },
+                      )),
             ),
           )
         ],
